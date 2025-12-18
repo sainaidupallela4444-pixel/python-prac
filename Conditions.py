b3c21f9bb3795cdf2d63d20ff772e5df1feb6e56
sainaidupallela4444-pixel <sainaidupallela4444@gmail.com>
2025-12-18 12:36:09 +0530

    new

---
+++ Conditions.py
@@ -0,0 +1,9 @@
+Temperature = 15
+if Temperature > 30:
+    print ("it is warm")
+    print ("drink water")
+elif Temperature > 20:
+    print ("It nice")
+else:
+    print("it is cold")
+print("done")
